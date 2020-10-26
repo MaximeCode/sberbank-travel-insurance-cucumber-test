bc06970ef07da41c0a1592c712657101657ec85b
maxime <maxime.mose>
2020-10-27 02:00:01 +0300

    init

--- src/main/java/steps/ScenarioSteps.java
+++ src/main/java/steps/ScenarioSteps.java
@@ -1,6 +1,7 @@
 package steps;
 
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.ru.Когда;
 
 public class ScenarioSteps {
@@ -8,44 +9,60 @@ public class ScenarioSteps {
     InsurancePageSteps insurancePageSteps = new InsurancePageSteps();
     TravelInsurancePageSteps travelInsurancePageSteps = new TravelInsurancePageSteps();
     TravelInsuranceFirstFormPageSteps travelInsuranceFirstFormPageSteps = new TravelInsuranceFirstFormPageSteps();
+    TravelInsuranceSecondFormPageSteps travelInsuranceSecondFormPageSteps = new TravelInsuranceSecondFormPageSteps();
 
-    @Когда("^на главной странице в меню выбираем раздел {string}$")
+    @Когда("на главной странице в меню выбираем раздел {string}")
     public void на_главной_странице_в_меню_выбираем_раздел(String string) {
         mainPageSteps.selectMenuItem(string);
     }
 
-    @Когда("^на открывшемся на главной странице меню выбираем {string}$")
+    @Когда("на открывшемся на главной странице меню выбираем {string}")
     public void на_открывшемся_на_главной_странице_меню_выбираем(String string) {
         mainPageSteps.selectSubMenuItem(string);
     }
 
-    @Когда("^на странице {string} в блоке {string} нажимаем {string}$")
+    @Когда("на странице {string} в блоке {string} нажимаем \"Подробнее\"")
     public void на_странице_в_блоке_нажимаем(String string) {
         insurancePageSteps.selectMenuItem(string);
     }
 
-    @Когда("^на странице {string} проверяем наличие заголовка {string}$")
+    @Когда("на странице {string} проверяем наличие заголовка {string}")
     public void на_странице_проверяем_наличие_заголовка(String string) {
         travelInsurancePageSteps.checkTitle(string);
     }
 
-    @Когда("^на странице {string} нажимаем \"Оформить Онлайн\"$")
+    @Когда("на странице {string} нажимаем \"Оформить Онлайн\"")
     public void на_странице_нажимаем_Оформить_Онлайн(String string) {
         travelInsurancePageSteps.proceed();
     }
 
-    @Когда("^на вкладке \"Выбор полиса\" выбираем сумму страховой защиты {string}$")
+    @Когда("на вкладке \"Выбор полиса\" выбираем сумму страховой защиты {string}")
     public void на_вкладке_Выбор_полиса_выбираем_сумму_страховой_защиты(String string) {
         travelInsuranceFirstFormPageSteps.selectSum(string);
     }
 
-    @Когда("^на вкладке \"Выбор полиса\" выбираем сумму страховой защиты {string}$")
-    public void на_вкладке_Выбор_полиса_выбираем_сумму_страховой_защиты(String string) {
-        travelInsuranceFirstFormPageSteps.selectSum(string);
+    @Когда("на вкладке \"Выбор полиса\" нажимаем \"Оформить\"")
+    public void на_вкладке_Выбор_полиса_нажимаем_Оформить() {
+        travelInsuranceFirstFormPageSteps.selectSum("1");
     }
 
-    @Когда("^на вкладке \"Выбор полиса\" выбираем сумму страховой защиты {string}$")
-    public void на_вкладке_Выбор_полиса_выбираем_сумму_страховой_защиты(String string) {
-        travelInsuranceFirstFormPageSteps.selectSum(string);
+    @Когда("на вкладке \"Оформление\" заполняем поля данными:")
+    public void на_вкладке_Оформление_заполняем_поля_данными(DataTable dataTable) {
+//        travelInsuranceSecondFormPageSteps.fillSurname(dataTable);
+    }
+
+    @Когда("на вкладке \"Оформление\" проверяем правильность заполнения полей:")
+    public void на_вкладке_Оформление_проверяем_правильность_заполнения_полей(DataTable dataTable) {
+//        travelInsuranceSecondFormPageSteps.fillSurname(string);
+    }
+
+    @Когда("на вкладке \"Оформление\" нажимаем \"Продолжить\"")
+    public void на_вкладке_Оформление_нажимаем_Продолжить(String string) {
+        travelInsuranceSecondFormPageSteps.fillSurname(string);
+    }
+
+    @Когда("на вкладке \"Оформление\" проверяем, что появилось сообщение {string}")
+    public void на_вкладке_Оформление_проверяем_что_появилось_сообщение(String string) {
+        travelInsuranceSecondFormPageSteps.fillSurname(string);
     }
 }
